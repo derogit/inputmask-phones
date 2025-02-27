3207fc66a603704b7981dd846ac8a98dc143bb01
derogit <dentk2010@gmail.com>
2025-02-27 09:43:38 +0200

    added validation

--- js/mask.js
+++ js/mask.js
@@ -1,31 +1,71 @@
-const mask = (selector) => {
+const mask = (selector, options = {}) => {
+    const { autoValidate = false } = options;
+
+    function isValidInput(input) {
+        const cleanValue = input.value.replace(/\D/g, '');
+        let isValid = false;
+        
+        for (const item of maskList) {
+            const maskDigits = item.code.replace(/[^\d#]/g, '').length;
+            // Escape special characters and create pattern
+            const maskPattern = item.code
+                .replace(/[+]/g, '\\+')  // Escape +
+                .replace(/[#]/g, '\\d')  // Replace # with \d
+                .replace(/ /g, '\\s?');  // Make spaces optional
+            const regex = new RegExp(`^${maskPattern}$`);
+            
+            if (cleanValue.length === maskDigits && 
+                regex.test(input.value.replace(/[- )(]/g, ''))) {
+                isValid = true;
+                break;
+            }
+        }
+        
+        return {
+            isValid,
+            value: input.value,
+            cleanedValue: cleanValue
+        };
+    }
+
     function setMask() {
         let matrix = '+###############';
+        const cleanPhone = this.value.replace(/[\s#-)(]/g, '');
 
         maskList.forEach(item => {
-            let code = item.code.replace(/[\s#]/g, ''),
-                phone = this.value.replace(/[\s#-)(]/g, '');
-
-            if (phone.includes(code)) {
-                console.log(phone, code);
+            let code = item.code.replace(/[\s#]/g, '');
+            if (cleanPhone.includes(code)) {
                 matrix = item.code;
             }
         });
 
-        let i = 0,
-            val = this.value.replace(/\D/g, '');
+        let i = 0;
+        const val = this.value.replace(/\D/g, '');
 
         this.value = matrix.replace(/(?!\+)./g, function(a) {
             return /[#\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? '' : a;
         });
+
+        if (autoValidate) {
+            const validation = isValidInput(this);
+            this.classList.toggle('valid', validation.isValid);
+            this.classList.toggle('invalid', !validation.isValid);
+        }
     }
 
-    let inputs = document.querySelectorAll(selector);
+    const inputs = document.querySelectorAll(selector);
 
     inputs.forEach(input => {
         if (!input.value) input.value = '+';
+        
+        input.validate = function() {
+            return isValidInput(this);
+        };
+
         input.addEventListener('input', setMask);
         input.addEventListener('focus', setMask);
         input.addEventListener('blur', setMask);
     });
+
+    return inputs;
 };
